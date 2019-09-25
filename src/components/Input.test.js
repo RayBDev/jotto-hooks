c3a09f52b6b2f40b9bb72e9a41b1cd5097e6e638
RayB <raybernard1337@gmail.com>
2019-09-25 07:21:06 -0600

    Added proptypes and updated test

--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import { findByTestAttr } from "../../test/testUtils";
+import { findByTestAttr, checkProps } from "../../test/testUtils";
 import Input from "./Input";
 
 /**
  * setup function for app component
  * @returns {ShallowWrapper}
  */
-const setup = () => {
-  return shallow(<Input />);
+const setup = (secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 
 test("Input renders without error", () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-input");
   expect(component.length).toBe(1);
+});
+
+test("Check prop types", () => {
+  const expectedProps = { secretWord: "party" };
+  checkProps(Input, expectedProps);
 });
