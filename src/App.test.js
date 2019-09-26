99c3d36864655044ca19995ce19ea8892f28c8a4
RayB <raybernard1337@gmail.com>
2019-09-26 08:15:56 -0600

    Added getSecretWord action and tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { findByTestAttr } from "../test/testUtils";
 import App from "./App";
 
 /**
  * setup function for app component
- * @returns {ShallowWrapper}
+ * @returns {MountWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  return mount(<App />);
 };
 
 test("App renders without error", () => {
