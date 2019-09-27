66dd790797d9ba6a4aa56a5117348385234f2f7d
RayB <raybernard1337@gmail.com>
2019-09-27 12:50:37 -0600

    Added a spinner during ajax request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+
 import "./App.css";
 import hookActions from "./actions/hookActions";
-
 import reducer from "./reducers/hookReducers";
+import Input from "./components/Input";
 
 function App() {
   const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
@@ -14,7 +15,22 @@ function App() {
     hookActions.getSecretWord(setSecretWord);
   }, []);
 
-  return <div data-test="component-app"></div>;
+  if (!state.secretWord) {
+    return (
+      <div className="container" data-test="spinner">
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <p>Loading secret word</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container" data-test="component-app">
+      <Input secretWord={state.secretWord} />
+    </div>
+  );
 }
 
 export default App;
